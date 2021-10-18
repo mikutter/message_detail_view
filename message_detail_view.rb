a3c133ef8170ccad6d842f772da78542976a8b5a
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2021-10-18 22:33:08 +0900

    Steal style_generator from gtk_postbox.

--- message_detail_view.rb
+++ message_detail_view.rb
@@ -48,24 +48,29 @@ Plugin.create(:message_detail_view) do
     end
   end
 
-  style = -> do
-    Gtk::Style.new().tap do |bg_style|
-      color = UserConfig[:mumble_basic_bg]
-      bg_style.set_bg(Gtk::STATE_ACTIVE, *color)
-      bg_style.set_bg(Gtk::STATE_NORMAL, *color)
-      bg_style.set_bg(Gtk::STATE_SELECTED, *color)
-      bg_style.set_bg(Gtk::STATE_PRELIGHT, *color)
-      bg_style.set_bg(Gtk::STATE_INSENSITIVE, *color) end end
-
   message_fragment :body, "body" do
     set_icon Skin[:message]
     container = Gtk::HBox.new
-    textview = Gtk::IntelligentTextview.new(model.description, 'font' => :mumble_basic_font, style: style)
+    textview = Gtk::IntelligentTextview.new(model.description, { 'font' => :mumble_basic_font })
+    textview.hexpand = true
+    textview.style_generator = get_style_provider
+    textview.bg_modifier
     vscrollbar = Gtk::VScrollbar.new
     textview.set_scroll_adjustment(nil, vscrollbar.adjustment)
     container.add textview
     container.closeup vscrollbar
     nativewidget container
   end
-end
 
+  def get_style_provider
+    color = UserConfig[:mumble_basic_bg]
+    Gtk::CssProvider.new.tap do |provider|
+      provider.load_from_data(<<~CSS)
+        *, *:active, *:disabled, *:hover, *:focus {
+          background-color: rgb(#{color[0] / 256}, #{color[1] / 256}, #{color[2] / 256});
+        }
+      CSS
+    end
+  end
+
+end
